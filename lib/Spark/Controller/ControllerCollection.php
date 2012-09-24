0a09e0d940276050d0587ec458b35afc7d998598
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-09-24 09:12:39 +0200

    Add 'resources' method for easy definition of restful routes

--- lib/Spark/Controller/ControllerCollection.php
+++ lib/Spark/Controller/ControllerCollection.php
@@ -9,4 +9,32 @@ class ControllerCollection extends \Silex\ControllerCollection
         $callback($this);
         return $this;
     }
+
+    function resources($resourceName)
+    {
+        $this->get("/$resourceName", "$resourceName#index")
+             ->bind("{$resourceName}_index");
+
+        $this->get("/$resourceName/new", "$resourceName#new")
+             ->bind("{$resourceName}_new");
+
+        $this->get("/$resourceName/{id}", "$resourceName#show")
+             ->bind("{$resourceName}_show");
+
+        $this->get("/$resourceName/{id}/edit", "$resourceName#edit")
+             ->bind("{$resourceName}_edit");
+
+        $this->post("/$resourceName", "$resourceName#create")
+             ->bind("{$resourceName}_create");
+
+        $this->put("/$resourceName/{id}", "$resourceName#update")
+             ->bind("{$resourceName}_update");
+
+        $this->delete("/$resourceName/{id}", "$resourceName#delete")
+             ->bind("{$resourceName}_delete");
+    }
+
+    function resource($resourceName)
+    {
+    }
 }
