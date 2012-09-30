6d8c3225286276d572c29094c578b45af9fadc00
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-09-30 11:16:02 +0200

    Make controller name configurable for resource routes

--- lib/Spark/Controller/ControllerCollection.php
+++ lib/Spark/Controller/ControllerCollection.php
@@ -6,41 +6,49 @@ class ControllerCollection extends \Silex\ControllerCollection
 {
     function draw(callable $callback)
     {
+        if ($callback instanceof \Closure) {
+            $callback = $callback->bindTo($this);
+        }
+
         $callback($this);
         return $this;
     }
 
-    function resources($resourceName)
+    function resources($resourceName, $options = [])
     {
-        $this->get("/$resourceName", "$resourceName#index")
+        $controller = @$options['controller'] ?: $resourceName;
+
+        $this->get("/$resourceName", "$controller#index")
              ->bind("{$resourceName}_index");
 
-        $this->get("/$resourceName/new", "$resourceName#new")
+        $this->get("/$resourceName/new", "$controller#new")
              ->bind("{$resourceName}_new");
 
-        $this->get("/$resourceName/{id}", "$resourceName#show")
+        $this->get("/$resourceName/{id}", "$controller#show")
              ->bind("{$resourceName}_show");
 
-        $this->get("/$resourceName/{id}/edit", "$resourceName#edit")
+        $this->get("/$resourceName/{id}/edit", "$controller#edit")
              ->bind("{$resourceName}_edit");
 
-        $this->post("/$resourceName", "$resourceName#create")
+        $this->post("/$resourceName", "$controller#create")
              ->bind("{$resourceName}_create");
 
-        $this->put("/$resourceName/{id}", "$resourceName#update")
+        $this->put("/$resourceName/{id}", "$controller#update")
              ->bind("{$resourceName}_update");
 
-        $this->delete("/$resourceName/{id}", "$resourceName#destroy")
+        $this->delete("/$resourceName/{id}", "$controller#destroy")
              ->bind("{$resourceName}_destroy");
     }
 
-    function resource($resourceName)
+    function resource($resourceName, $options = [])
     {
-        $this->get("/$resourceName", "$resourceName#show");
-        $this->get("/$resourceName/new", "$resourceName#new");
-        $this->get("/$resourceName/edit", "$resourceName#edit");
-        $this->post("/$resourceName", "$resourceName#create");
-        $this->put("/$resourceName", "$resourceName#update");
-        $this->delete("/$resourceName", "$resourceName#destroy");
+        $controller = @$options['controller'] ?: $resourceName;
+
+        $this->get("/$resourceName", "$controller#show");
+        $this->get("/$resourceName/new", "$controller#new");
+        $this->get("/$resourceName/edit", "$controller#edit");
+        $this->post("/$resourceName", "$controller#create");
+        $this->put("/$resourceName", "$controller#update");
+        $this->delete("/$resourceName", "$controller#destroy");
     }
 }
