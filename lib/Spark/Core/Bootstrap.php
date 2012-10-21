866484b96a116ec528f5072209ae8b1d720f67f1
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-10-21 19:11:08 +0200

    Add start() and end() hooks to Bootstrap

--- lib/Spark/Core/Bootstrap.php
+++ lib/Spark/Core/Bootstrap.php
@@ -6,12 +6,19 @@ use Spark\Application;
 
 class Bootstrap
 {
+    # Runs before all configuration is done.
+    static function start(Application $app){}
+
+    # Runs after all configuration is done, before run() is called.
+    static function end(Application $app){}
+
     static function bootstrap($root, $environment)
     {
         $app = new Application;
         $app['spark.env'] = $environment;
         $app['spark.root'] = $root;
-        $app['spark.data_dir'] = "$root/data";
+
+        static::start($app);
 
         $configFiles = [
             "$root/config/application.php",
@@ -24,6 +31,8 @@ class Bootstrap
                 require_once($config);
             }
         }
+
+        static::end($app);
 
         return $app;
     }
--- lib/Spark/Core/Command/templates/config/bootstrap.php
+++ lib/Spark/Core/Command/templates/config/bootstrap.php
@@ -2,8 +2,21 @@
 
 namespace __AppName__;
 
+use Spark\Application;
+
 class Bootstrap extends \Spark\Core\Bootstrap
 {
+    static function start(Application $app)
+    {
+        # Include your code here, which does things before
+        # configuration files are included.
+    }
+
+    static function end(Application $app)
+    {
+        # Include your code here, which runs after all configuration has
+        # happend.
+    }
 }
 
 return Bootstrap::bootstrap(__DIR__ . '/../', @$_SERVER['SPARK_ENV'] ?: "development");
