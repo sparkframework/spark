8a2498d745764189243e20aae9e4ae3698450479
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-10-22 09:04:14 +0200

    Mixed up development and production config templates

--- lib/Spark/Core/Command/templates/config/environments/development.php
+++ lib/Spark/Core/Command/templates/config/environments/development.php
@@ -1,3 +1,12 @@
 <?php
 
+$app['debug'] = true;
+
+# Asset Pipeline Configuration
+
+# Generate assets dynamically in development
+$app['pipe.use_precompiled'] = false;
+
+# Turn off asset compression
+$app['pipe.debug'] = true;
 
